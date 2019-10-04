e776c5a4bc16233a44cb42dde06224ea44d5d16c
Nicole Marie Garcia <ngarcia@linprog3.cs.fsu.edu>
2019-10-04 16:19:58 -0400

    Adding Jet Class

---
+++ Jet.cpp
@@ -0,0 +1,37 @@
+#include "Jet.h"
+
+Jet::Jet()
+{
+
+
+}
+
+
+Jet::Jet()
+{
+
+
+}
+
+
+Jet::~Jet() = default;
+
+int Jet::getNumberOfEngines()
+{
+
+}
+
+void Jet::setNumberOfEngines()
+{
+
+}
+
+
+double Jet::mileageEstimate()
+{
+
+}
+
+
+
+
---
+++ Jet.h
@@ -0,0 +1,23 @@
+// Created by Nicole Garcia on 10/4/19
+
+
+#ifndef DRIVINGSIMULATOR_JET_H
+#define DRIVINGSIMULATOR_JET_H
+
+#include "PoweredVehicle.h"
+
+class Jet : public PoweredVehichle {
+
+private:
+	int numberOfEngines;
+
+public:
+	explicit Jet(string brand, string model, string fuelType, int numberOfEngines);
+	virtual ~Jet();
+	int getNumOfEngines();
+	void setNumberOfEngines(int numberOfEngines);
+	virtual double mileageEstimate(double time);
+
+};
+
+#endif //DRIVINGSIMULATOR_JET_H
